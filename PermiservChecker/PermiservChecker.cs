4adf2300eb88032203d6143b54ebfa8823222e8f
Kevin White <kwhite@northampton.gov.uk>
2020-05-22 16:41:55 +0100

    Loop to ensure min of 15 cases processed

--- PermiservChecker/PermiservChecker.cs
+++ PermiservChecker/PermiservChecker.cs
@@ -13,7 +13,7 @@ namespace PermiservChecker
         [FunctionName("PermiservChecker")]
         public static void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"PermiservChecker v1.2 executed at: {DateTime.Now}");
+            log.LogInformation($"PermiservChecker v1.3 executed at: {DateTime.Now}");
             String[] cases = null;
             String filter = "gws-with-permiserv";
             int pages = 0;
@@ -53,122 +53,134 @@ namespace PermiservChecker
                 log.LogError("CXM API get filtered cases (" + filter + ") exception " + error.Message);
             }
 
-            requestParameters = "key=" + cxmAPIKey + "&page=" + pages.ToString();
-            request = new HttpRequestMessage(HttpMethod.Get, "/api/service-api/norbert/filters/" + filter + "/summaries" + "?" + requestParameters);
 
-            try
+            int casesTransitioned = 0;
+            for(int currentPage = pages; currentPage > 0; currentPage--)
             {
-                HttpResponseMessage response = cxmClient.SendAsync(request).Result;
-                if (response.IsSuccessStatusCode)
+                if (casesTransitioned>14)
                 {
-                    HttpContent responseContent = response.Content;
-                    String responseString = responseContent.ReadAsStringAsync().Result;
-                    JObject caseSearch = JObject.Parse(responseString);
-                    int totalCases = (int)caseSearch.SelectToken("total");
-                    int numOfCases = (int)caseSearch.SelectToken("num_items");
-                    cases = new String[numOfCases];
-                    if (totalCases == 0)
-                    {
-                        log.LogInformation("No cases to be processed");
-                    }
-                    else
-                    {
-                        for (int currentCase = 0; currentCase < numOfCases; currentCase++)
-                        {
-                            cases[currentCase] = (String)caseSearch.SelectToken("items[" + currentCase + "].reference");               
-                        }
-                    }
+                    break;
                 }
-                else
-                {
-                    log.LogError("CXM API get filtered cases (" + filter + ") failed");
-                }
-            }
-            catch (Exception error)
-            {
-                log.LogError("CXM API get filtered cases (" + filter + ") error : " + error.Message);
-            }
+                requestParameters = "key=" + cxmAPIKey + "&page=" + currentPage.ToString();
+                request = new HttpRequestMessage(HttpMethod.Get, "/api/service-api/norbert/filters/" + filter + "/summaries" + "?" + requestParameters);
 
-            try
-            {
-                HttpClient permiservClient = new HttpClient();
-                permiservClient.BaseAddress = new Uri(permiservEndpoint);
-                for (int currentCase = 0; currentCase < cases.Length; currentCase++)
+                try
                 {
-                    requestParameters = "apiKey=" + permiservAPIKey + "&dataType=" + "get" + "&callType=" + "search" + "&councilJobNumber=" + cases[currentCase];
-                    request = new HttpRequestMessage(HttpMethod.Get, "/api/" + "?" + requestParameters);
-                    HttpResponseMessage response = permiservClient.SendAsync(request).Result;
+                    HttpResponseMessage response = cxmClient.SendAsync(request).Result;
                     if (response.IsSuccessStatusCode)
                     {
                         HttpContent responseContent = response.Content;
                         String responseString = responseContent.ReadAsStringAsync().Result;
                         JObject caseSearch = JObject.Parse(responseString);
-                        int numRows = 0;
-                        if (caseSearch.SelectToken("Success").ToString().Equals("true"))
-                        {                      
-                           numRows = (int)caseSearch.SelectToken("['Num Rows']");
-
-                            if(numRows==0)
+                        int totalCases = (int)caseSearch.SelectToken("total");
+                        int numOfCases = (int)caseSearch.SelectToken("num_items");
+                        cases = new String[numOfCases];
+                        if (totalCases == 0)
+                        {
+                            log.LogInformation("No cases to be processed");
+                        }
+                        else
+                        {
+                            for (int currentCase = 0; currentCase < numOfCases; currentCase++)
                             {
-                                requestParameters = "key=" + cxmAPIKey;
-                                request = new HttpRequestMessage(HttpMethod.Post, "/api/service-api/norbert/case/" + cases[currentCase] + "/transition/" + "with-veolia" + "?" + requestParameters);
-                                log.LogWarning("Permiserv record not found, transitioning " + cases[currentCase] + " back to with-veolia for resubmission");
-                                try
+                                cases[currentCase] = (String)caseSearch.SelectToken("items[" + currentCase + "].reference");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        log.LogError("CXM API get filtered cases (" + filter + ") failed");
+                    }
+                }
+                catch (Exception error)
+                {
+                    log.LogError("CXM API get filtered cases (" + filter + ") error : " + error.Message);
+                }
+
+                try
+                {
+                    HttpClient permiservClient = new HttpClient();
+                    permiservClient.BaseAddress = new Uri(permiservEndpoint);
+                    for (int currentCase = 0; currentCase < cases.Length; currentCase++)
+                    {
+                        requestParameters = "apiKey=" + permiservAPIKey + "&dataType=" + "get" + "&callType=" + "search" + "&councilJobNumber=" + cases[currentCase];
+                        request = new HttpRequestMessage(HttpMethod.Get, "/api/" + "?" + requestParameters);
+                        HttpResponseMessage response = permiservClient.SendAsync(request).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            HttpContent responseContent = response.Content;
+                            String responseString = responseContent.ReadAsStringAsync().Result;
+                            JObject caseSearch = JObject.Parse(responseString);
+                            int numRows = 0;
+                            if (caseSearch.SelectToken("Success").ToString().Equals("true"))
+                            {
+                                numRows = (int)caseSearch.SelectToken("['Num Rows']");
+                                if (numRows == 0)
                                 {
-                                    response = cxmClient.SendAsync(request).Result;
-                                    if (!response.IsSuccessStatusCode)
+                                    requestParameters = "key=" + cxmAPIKey;
+                                    request = new HttpRequestMessage(HttpMethod.Post, "/api/service-api/norbert/case/" + cases[currentCase] + "/transition/" + "with-veolia" + "?" + requestParameters);
+                                    try
                                     {
-                                        log.LogError("Permiserv transition error " + cases[currentCase] + " error : Unsuccessful status code");
+                                        response = cxmClient.SendAsync(request).Result;
+                                        if (response.IsSuccessStatusCode)
+                                        {
+                                            log.LogWarning("Permiserv record not found, transitioning " + cases[currentCase] + " back to with-veolia for resubmission");
+                                            casesTransitioned++;
+                                        }
+                                        else
+                                        {
+                                            log.LogError("Permiserv transition error " + cases[currentCase] + " error : Unsuccessful status code");
+                                        }
+                                    }
+                                    catch (Exception error)
+                                    {
+                                        log.LogError("Permiserv transition error " + cases[currentCase] + " error : " + error.Message);
                                     }
                                 }
-                                catch (Exception error)
-                                {
-                                    log.LogError("Permiserv transition error " + cases[currentCase] + " error : " + error.Message);
-                                }
-                            }
 
-                            for (int currentRow = 0; currentRow < numRows; currentRow++)
-                            {
-                                 if (caseSearch.SelectToken("Result[" + currentRow + "].state").ToString().Equals("Dispatched")||
-                                     caseSearch.SelectToken("Result[" + currentRow + "].state").ToString().Equals("Replaced"))
-                                 {
+                                for (int currentRow = 0; currentRow < numRows; currentRow++)
+                                {
+                                    if (caseSearch.SelectToken("Result[" + currentRow + "].state").ToString().Equals("Dispatched") ||
+                                        caseSearch.SelectToken("Result[" + currentRow + "].state").ToString().Equals("Replaced"))
+                                    {
                                         requestParameters = "key=" + cxmAPIKey;
                                         request = new HttpRequestMessage(HttpMethod.Post, "/api/service-api/norbert/case/" + cases[currentCase] + "/transition/" + "active-subscription" + "?" + requestParameters);
                                         try
                                         {
                                             response = cxmClient.SendAsync(request).Result;
-                                            if (!response.IsSuccessStatusCode)
-                                            {
-                                                log.LogError("Permiserv transition error " + cases[currentCase] + " error : Unsuccessful status code");
-                                            }
-                                        else
+                                            if (response.IsSuccessStatusCode)
                                             {
                                                 log.LogInformation(cases[currentCase] + " transitioned to active-subscription");
+                                                casesTransitioned++;                                               
+                                            }
+                                            else
+                                            {
+                                                log.LogError("Permiserv transition error " + cases[currentCase] + " error : Unsuccessful status code");
                                             }
                                         }
                                         catch (Exception error)
                                         {
                                             log.LogError("Permiserv transition error " + cases[currentCase] + " error : " + error.Message);
                                         }
-                                 }
-                                else
-                                {
-                                    log.LogInformation(cases[currentCase] + " ignored - Permiserv status is : " + caseSearch.SelectToken("Result[" + currentRow + "].state").ToString());
+                                    }
+                                    else
+                                    {
+                                        log.LogInformation(cases[currentCase] + " ignored - Permiserv status is : " + caseSearch.SelectToken("Result[" + currentRow + "].state").ToString());
+                                    }
                                 }
                             }
-                        }
-                        else
-                        {
-                            log.LogError("Permiserv API case search for " + cases[currentCase] +  " error : " + caseSearch.SelectToken("Errors[" + currentCase + "]"));
+                            else
+                            {
+                                log.LogError("Permiserv API case search for " + cases[currentCase] + " error : " + caseSearch.SelectToken("Errors[" + currentCase + "]"));
+                            }
                         }
                     }
                 }
-            }
-            catch (Exception error)
-            {
-                log.LogError("Permiserv API get error " + error.Message);
-            }
-            
+                catch (Exception error)
+                {
+                    log.LogError("Permiserv API get error " + error.Message);
+                }
+            }          
         }
     }
 }
